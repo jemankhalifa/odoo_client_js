bd42e39f495661a47fcdc031466e333c2622336e
Eman Khalifa <khalifaeman4@gmail.com>
2021-10-09 13:33:53 +0200

    Adding button to Odoo list view actions buttons

---
+++ test_client_app/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Test Client JS app",
+
+    'summary': """
+    		Add a button in the Odoo list view actions buttons
+        """,
+
+    'description': """
+    """,
+
+    'author': "Eman khalifa",
+    'website': "http://www.emankhalifa.com",
+    'version':'13.0',
+    'category': 'Accounting',
+    'depends': ['base','account'],
+    'data': [
+
+
+    ],
+    'qweb': [
+        "static/xml/ListAddButton.xml"
+    ],
+}
---
+++ test_client_app/static/js/ListController.js
@@ -0,0 +1,32 @@
+odoo.define('test_client_app.ButtonsInList', function (require) {
+    "use strict";
+
+    var ListController = require("web.ListController");
+    
+    
+    var includeDict = {
+        renderButtons: function () {
+            this._super.apply(this, arguments);
+            if (this.modelName === "account.account") {
+                var coa_button = this.$buttons.find('.o_list_button');
+                coa_button.on('click', this.proxy('o_button_click'));
+            }
+        },
+        o_button_click: function(e){
+
+            var action = {
+                    type: 'ir.actions.client',
+                    name: 'New Custom View',
+                    tag: 'account_custom_view',
+                  
+                };
+                this.do_action(action);
+    
+        }
+    };
+    ListController.include(includeDict);
+    
+    
+    
+    });
+    
